df485fd4f4eaa9cf640f5941c11ea3ffb3dfe309
Levani Tevdoradze <levani.tevdoradze@epita.fr>
2019-07-02 14:12:30 +0200

    Added size and rank init

--- src/main.cc
+++ src/main.cc
@@ -69,6 +69,11 @@ int main(int argc, char** argv)
   // Parsing arguments
   if (argc != 5)
     return print_usage_and_exit();
+  int world_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  // Get the rank of the process
+  int world_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
   datas = read_file(std::string(argv[1]));
   /* TODO
