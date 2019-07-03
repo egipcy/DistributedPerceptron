6053672a58d60eed399465f1ca1ac441840e174d
Nebraw <b.fredericmoreau@gmail.com>
2019-07-03 16:28:52 +0200

    [clean]

--- src/process.cc
+++ src/process.cc
@@ -18,7 +18,7 @@ void Process::elect_president()
 {
   int id = this->id_;
   int prevId = id - 1 < 0 ? this->world_size_ - 1 : id - 1;
-  int nextId = id + 1 >= this->world_size_ - 1 ? 0 : id + 1;
+  int nextId = id + 1 >= this->world_size_ ? 0 : id + 1;
   MPI_Send(&id, 1, MPI_INT, prevId, 0, MPI_COMM_WORLD);
   MPI_Send(&id, 1, MPI_INT, nextId, 0, MPI_COMM_WORLD);
 }
