2c4d52989c280d5632959fdc262d7234ec5cd625
Levani Tevdoradze <levani.tevdoradze@epita.fr>
2019-07-07 13:03:07 +0200

    Updated main

--- src/header.hh
+++ src/header.hh
@@ -1,5 +1,5 @@
 #ifndef ENV
 #define ENV
-#define __ARGC__ 4
+#define __ARGC__ 5
 
 #endif ENV
--- src/main.cc
+++ src/main.cc
@@ -16,13 +16,36 @@ int main(int argc, char** argv) {
     std::cerr << "Wrong number of arguments" << std::endl;
     std::cerr << "Arguments : data's path, master/worker ratio, n_epoch"
     return 1
-  }  
+  }
 
   MPI_Init(NULL, NULL);
   int rank, w_size;
   MPI_Comm_size(MPI_COMM_WORLD, &w_size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   Process p = Process(rank, w_size, argv[1], argv[2], argv[3]);
+  p.elect_president();
+  if (p.get_type() == Type::President)
+  {
+
+  }
+  
+  MPI_Status status;
+  int number_amount, flag;
+  while (!p.has_ended())
+  {
+    if (p.get_type() == Type::President)
+    {
+      MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &flag, &status);
+    }
+    else if (p.get_type() == Type::Worker)
+    {
+      
+    }
+    else {
+
+    }
+    
 
 
+  }
 }
