29da1c83c640f1e6ceaa0827abc8d040e719e778
Levani Tevdoradze <levani.tevdoradze@epita.fr>
2019-07-07 16:33:22 +0200

    Updated main

--- src/main.cc
+++ src/main.cc
@@ -32,11 +32,12 @@ int main(int argc, char** argv)
   }
   
   MPI_Status status;
-  int number_amount, flag;
+  int number_amount;
   while (!p.has_ended())
   {
-    std::cout << "ok" << std::endl;
+    MPI_Status status;
     int flag = false;
+    int recv_number=0;
     while (!flag)
     {
       if (p.get_type() == Type::President)
@@ -52,19 +53,21 @@ int main(int argc, char** argv)
         MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &flag, &status);
       }
     }
+    MPI_Recv(&recv_number,1, MPI_INT, status.MPI_SOURCE, status.MPI_TAG, MPI_COMM_WORLD, &status);
+    int number = 1;
     switch(status.MPI_TAG)
     {
-      case 0:
-        std::cout << rank << std::endl;
-        break;
       case 1:
+        std::cout << rank << std::endl;
+        MPI_Send(&number, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
+        break;
+      case 2:
         std::cout << "President recieved a response from "
           << status.MPI_SOURCE
           << std::endl;
+        break;
       default:
-        std::cout << "Tag doesn\'t match" << std::endl;
+        std::cout << "Tag doesn\'t match: " << status.MPI_TAG << " " << status.MPI_SOURCE << std::endl;
     }
-    int number = 1;
-    MPI_Send(&number, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
   }
 }
