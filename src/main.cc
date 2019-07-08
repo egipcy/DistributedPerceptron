1381a48cf851ec7a4dbf4f33751d1fc9bab5aabd
Levani Tevdoradze <levani.tevdoradze@epita.fr>
2019-07-08 16:14:32 +0200

    udated process

--- src/main.cc
+++ src/main.cc
@@ -57,9 +57,9 @@ int main(int argc, char** argv)
       MPI_Get_count(&status, MPI_DOUBLE, &count);
       std::vector<double> w(count);
       MPI_Recv(w.data(), count, MPI_DOUBLE, status.MPI_SOURCE, status.MPI_TAG, MPI_COMM_WORLD, &status);
-      std::vector<double> p(count);
-      MPI_Recv(p.data(), count, MPI_DOUBLE, status.MPI_SOURCE, Tag::BiasesMatrix, MPI_COMM_WORLD, &status);
-      p.set_weights_biases(w, p);
+      std::vector<double> b(count);
+      MPI_Recv(b.data(), count, MPI_DOUBLE, status.MPI_SOURCE, Tag::BiasesMatrix, MPI_COMM_WORLD, &status);
+      p.set_weights_biases(w, b);
       std::pair<Matrix, Matrix> g = p.get_gradient(); //weights, biases
       auto weights = serialize(g.first);
       auto biases = serialize(g.second);
