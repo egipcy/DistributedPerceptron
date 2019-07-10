ede8529e4615aa4ad6edcf56ec677f79f1d6126b
Levani Tevdoradze <levani.tevdoradze@epita.fr>
2019-07-10 12:20:28 +0200

    Updated main

--- src/main.cc
+++ src/main.cc
@@ -25,7 +25,7 @@ int main(int argc, char** argv)
   int rank, world_size;
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
+  
   Process p = Process(rank, world_size, argv[1], argv[2]);
 
   //std::cout << p.get_rank() << " Election..." << std::endl;
